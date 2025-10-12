bc1674bf18c8267fa08fae2ee56ab42d414a6744
Georgiy Beloklokov <georgiy.reus@gmail.com>
2025-10-12 12:48:48 +0300

    feat: Implement global rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,30 @@ import { MediaModule } from './media/media.module';
 import { HealthModule } from './health/health.module';
 import { GracefulShutdownService } from './graceful-shutdown.service';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, MediaModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 1 minute in milliseconds
+        limit: 100, // 100 requests
+      },
+    ]),
+    PrismaModule,
+    MediaModule,
+    HealthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, GracefulShutdownService],
+  providers: [
+    AppService,
+    GracefulShutdownService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
