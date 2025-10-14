64ef455f79fe1e2d61ad318760b9a8ee98e69c04
Georgiy Beloklokov <georgiy.reus@gmail.com>
2025-10-14 19:32:52 +0300

    refactor: improve db performance and dx by adding connection limits, volumes and indexes

--- prisma/migrations/20251014150215_init/migration.sql
+++ prisma/migrations/20251014150215_init/migration.sql
@@ -23,10 +23,19 @@ CREATE TABLE "Media" (
 );
 
 -- CreateIndex
+CREATE INDEX "Media_createdAt_idx" ON "Media"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Media_status_createdAt_idx" ON "Media"("status", "createdAt");
+
+-- CreateIndex
 CREATE INDEX "Media_uploaderId_idx" ON "Media"("uploaderId");
 
 -- CreateIndex
-CREATE INDEX "Media_status_idx" ON "Media"("status");
+CREATE INDEX "Media_mimeType_idx" ON "Media"("mimeType");
 
 -- CreateIndex
-CREATE INDEX "Media_createdAt_idx" ON "Media"("createdAt");
+CREATE INDEX "Media_size_idx" ON "Media"("size");
+
+-- CreateIndex
+CREATE INDEX "Media_name_idx" ON "Media"("name");
